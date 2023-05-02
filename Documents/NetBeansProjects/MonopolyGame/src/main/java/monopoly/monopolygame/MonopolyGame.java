406e094e0760ad482f6f4f5a40edda325126dcbf
GlobalDuc <96855379+GlobalDuc@users.noreply.github.com>
2023-05-02 07:47:13 +0700

    init game

---
+++ Documents/NetBeansProjects/MonopolyGame/src/main/java/monopoly/monopolygame/MonopolyGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package monopoly.monopolygame;
+
+/**
+ *
+ * @author PC 
+ */
+public class MonopolyGame {
+                      
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
