c0a4924280c2a0e0750df1c2b89a6429ea02994e
TranThanhHuy2101 <0902637458tth@gmail.com>
2023-05-05 20:48:48 +0700

    add Dice

--- Documents/NetBeansProjects/MonopolyGame/src/main/java/monopoly/monopolygame/Dice.java
+++ Documents/NetBeansProjects/MonopolyGame/src/main/java/monopoly/monopolygame/Dice.java
@@ -35,19 +35,19 @@ public class Dice extends JPanel implements ActionListener {
       g.setColor(Color.black);
       g.drawRect(x, y, diceSize, diceSize);
       if (faceValue == 1 || faceValue == 3 || faceValue == 5) {
-         g.fillOval(x + diceSize / 2 - 5, y + diceSize / 2 - 5, 10, 10); // trung tâm
+         g.fillOval(x + diceSize / 2 - 5, y + diceSize / 2 - 5, 10, 10); 
       }
       if (faceValue == 2 || faceValue == 3 || faceValue == 4 || faceValue == 5 || faceValue == 6) {
-         g.fillOval(x + 10, y + 10, 10, 10); // Góc trên bên trái
-         g.fillOval(x + diceSize - 20, y + diceSize - 20, 10, 10); // Góc dưới bên phải
+         g.fillOval(x + 10, y + 10, 10, 10); 
+         g.fillOval(x + diceSize - 20, y + diceSize - 20, 10, 10);
       }
       if (faceValue == 4 || faceValue == 5 || faceValue == 6) {
-         g.fillOval(x + 10, y + diceSize - 20, 10, 10); // Góc dưới bên trái
-         g.fillOval(x + diceSize - 20, y + 10, 10, 10); // Góc trên bên phải
+         g.fillOval(x + 10, y + diceSize - 20, 10, 10); 
+         g.fillOval(x + diceSize - 20, y + 10, 10, 10); 
       }
       if (faceValue == 6) {
-         g.fillOval(x + 10, y + diceSize / 2 - 5, 10, 10); // Đoạn thẳng giữa bên trái
-         g.fillOval(x + diceSize - 20, y + diceSize / 2 - 5, 10, 10); // Đoạn thẳng giữa bên phải
+         g.fillOval(x + 10, y + diceSize / 2 - 5, 10, 10); 
+         g.fillOval(x + diceSize - 20, y + diceSize / 2 - 5, 10, 10); 
       }
    }
 
